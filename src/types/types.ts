eb4ff07614b8e2a0d2f2f89ba67ebeb1577682e6
thayawee <thayaweepinta@gmail.com>
2023-07-18 13:27:48 +0700

    add types menu-dto

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,15 @@
 export interface MenuDto {
   id: number
+  menu_name: string
+  description: string
+  menu_image_url: string
+  calories: number
+  process: string
+  nationality: string
+  healthy_concern: string
+  material: string
+  cooking_step: string
+  updated_at: Date
+  comment: string
+  rating: number
 }
