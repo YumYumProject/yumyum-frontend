2bd6af3f4e29843dcc2dd768aecb7dca6ed859c9
Bingutn <bingutn@gmail.com>
2023-07-18 14:12:08 +0700

    feat: add Home u

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import Search from './pages/Search'
+import Home from './pages/Home'
 
 function App() {
   return (
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+// link login,logout + register
+
+
+const Home = () => {
+    return
+    <>
+        <div class="box">
+            <button>Get Started</button>
+        </div>
+    </>
+}
+
+export default Home;
